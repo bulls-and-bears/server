fbc7d70150ed765f15dbc74c0a84ff2a6e73aca5
kkkwp <chxrryda@gmail.com>
2023-10-10 15:07:27 +0900

    build: 프로젝트 빌드

---
+++ src/main/java/com/shinhan/bullsandbears/BullsAndBearsApplication.java
@@ -0,0 +1,13 @@
+package com.shinhan.bullsandbears;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BullsAndBearsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BullsAndBearsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shinhan/bullsandbears/BullsAndBearsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shinhan.bullsandbears;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BullsAndBearsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
