3ec4f6a40fb332786b36ae737e0e53bbab0bd7bf
kkkwp <chxrryda@gmail.com>
2023-10-13 01:15:33 +0900

    Revert "feat: user 관련 속성 추가"
    This reverts commit 5835cb642fd0fb3e1d609bebdf95f76bf2ce9674.

--- src/main/java/com/shinhan/bullsandbears/domain/UserReportHistory.java
+++ src/main/java/com/shinhan/bullsandbears/domain/UserReportHistory.java
@@ -1,42 +1,32 @@
 package com.shinhan.bullsandbears.domain;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-
-import com.shinhan.bullsandbears.domain.user.User;
-
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserReportHistory {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "user_report_history_id")
-	private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name="user_report_history_id")
+  private Long id;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "user_id")
-	private User user;
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name="user_id")
+  private User user;
 
-	@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
-	@JoinColumn(name = "report_id")
-	private Report report;
+  @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+  @JoinColumn(name="report_id")
+  private Report report;
 
-	@Builder
-	public UserReportHistory(User user, Report report) {
-		this.user = user;
-		this.report = report;
-	}
+  @Builder
+  public UserReportHistory(User user, Report report){
+    this.user = user;
+    this.report = report;
+  }
 }
