73f95b3dcef772db7068477e5827db8de601c000
kkkwp <chxrryda@gmail.com>
2023-10-14 02:21:53 +0900

    feat: User 정보 받아오기

---
+++ src/main/java/com/shinhan/bullsandbears/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.shinhan.bullsandbears.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.shinhan.bullsandbears.auth.CustomOAuth2UserService;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+	private final CustomOAuth2UserService customOAuth2UserService;
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+			.csrf().disable()
+			.formLogin().disable()
+			.logout().disable()
+			.httpBasic().disable()
+			.headers().frameOptions().disable()
+
+			.and()
+			.authorizeRequests()
+			.antMatchers("/", "/oauth/**").permitAll()
+			.anyRequest().authenticated()
+
+			.and()
+			.oauth2Login()
+			.defaultSuccessUrl("/oauth/loginInfo", true)
+			.userInfoEndpoint()
+			.userService(customOAuth2UserService);
+
+		return http.build();
+	}
+}
--- src/main/java/com/shinhan/bullsandbears/domain/User.java
+++ src/main/java/com/shinhan/bullsandbears/domain/User.java
@@ -1,28 +1,39 @@
 package com.shinhan.bullsandbears.domain;
 
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-
-import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 @Entity
 @Getter
-@NoArgsConstructor(access= AccessLevel.PROTECTED)
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
 
-  @Id
-  private String email;
+	@Id
+	private String email;
 
-  private String name;
-  private LocalDateTime createdAt;
+	private String name;
+	private String provider;
+	private LocalDateTime createdAt;
 
-  @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-  private List<UserReportHistory> userReportHistoryList = new ArrayList<>();
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<UserReportHistory> userReportHistoryList = new ArrayList<>();
 
-
+	public User update(String name) {
+		this.name = name;
+		return this;
+	}
 }
