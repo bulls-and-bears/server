efed56bb44cef67c337cd6916dfc8ca7e7997c8b
younghyun-na <younghyunna@naver.com>
2023-10-14 19:49:51 +0900

    update: days 필드를 포함하도록 Duration enum 수정

--- src/main/java/com/shinhan/bullsandbears/domain/Duration.java
+++ src/main/java/com/shinhan/bullsandbears/domain/Duration.java
@@ -4,16 +4,18 @@ import lombok.Getter;
 
 @Getter
 public enum Duration {
-
-  SIXTH_MONTHS("6개월"),
-  ONE_YEAR("1년"),
-  TWO_YEARS("2년"),
-  THREE_YEARS("3년");
+  TWO_MONTHS("2개월", 60),
+  SIXTH_MONTHS("6개월", 180),
+  ONE_YEAR("1년", 365),
+  TWO_YEARS("2년", 730),
+  THREE_YEARS("3년", 1095);
 
   private final String label;
-
-  Duration(String label) {
+  private final int days;
+  
+  Duration(String label, int days) {
     this.label = label;
+    this.days = days;
   }
 
 }
