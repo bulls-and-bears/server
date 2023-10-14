02ee6357842b85f92114524985633d5d4f6a0edb
younghyun-na <younghyunna@naver.com>
2023-10-14 20:00:52 +0900

    feat: 금액에 따라 주식 데이터 조합하는 기능

--- src/main/java/com/shinhan/bullsandbears/report/ReportServiceImpl.java
+++ src/main/java/com/shinhan/bullsandbears/report/ReportServiceImpl.java
@@ -29,8 +29,8 @@ public class ReportServiceImpl implements ReportService {
     BigDecimal amount = request.getAmount();
 
     List<StockMaster> stocks = stockRepository.findAll();
-
     List<StockMaster> calculatedStocks = calculateLogic(stocks, duration.getDays());
+    List<StockMaster> optimalStocks = findOptimalStockCombination(calculatedStocks, amount);
 
     Report report = Report.builder()
             .duration(duration)
@@ -38,7 +38,7 @@ public class ReportServiceImpl implements ReportService {
             .createdAt(LocalDate.now())
             .build();
 
-    calculatedStocks.forEach(stock -> createStockReport(stock, report));
+    optimalStocks.forEach(stock -> createStockReport(stock, report));
 
     return new ReportDto.CreateResponse(report);
   }
@@ -84,5 +84,53 @@ public class ReportServiceImpl implements ReportService {
             .sorted(dividendComparator)
             .limit(10)
             .collect(Collectors.toList());
+  }
+
+  public List<StockMaster> findOptimalStockCombination(List<StockMaster> stocks, BigDecimal amount) {
+
+    int n = stocks.size();
+    List<List<BigDecimal>> dp = new ArrayList<>(n + 1);
+
+
+    for (int i = 0; i <= n; i++) {
+      List<BigDecimal> row = new ArrayList<>(amount.intValue() + 1);
+      for (int j = 0; j <= amount.intValue(); j++) {
+        row.add(BigDecimal.ZERO);
+      }
+      dp.add(row);
+    }
+
+    for (int i = 1; i <= n; i++) {
+      StockMaster stock = stocks.get(i - 1);
+      BigDecimal price = stock.getPrice();
+      BigDecimal dividend = stock.getDividendAmount();
+
+      for (int j = 0; j <= amount.intValue(); j++) {
+        BigDecimal maxWithoutSelect = dp.get(i - 1).get(j);
+
+        dp.get(i).set(j, maxWithoutSelect);
+
+        for (int k = 1; k * price.intValue() <= j; k++) {
+          BigDecimal selectMultipleTimes = dp.get(i - 1).get(j - k * price.intValue())
+                  .add(BigDecimal.valueOf(k).multiply(dividend));
+          
+          if (selectMultipleTimes.compareTo(dp.get(i).get(j)) > 0) {
+            dp.get(i).set(j, selectMultipleTimes);
+          }
+        }
+      }
+
+    }
+
+    int j = amount.intValue();
+    List<StockMaster> selectedStocks = new ArrayList<>();
+    for (int i = n; i > 0 && j > 0; i--) {
+      if (!dp.get(i).get(j).equals(dp.get(i - 1).get(j))) {
+        selectedStocks.add(stocks.get(i - 1));
+        j -= stocks.get(i - 1).getPrice().intValue();
+      }
+    }
+
+    return selectedStocks;
   }
 }
