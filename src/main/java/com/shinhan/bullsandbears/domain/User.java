9d97866bec3af948d583fd3da1280eb39569f954
kkkwp <chxrryda@gmail.com>
2023-10-16 15:08:13 +0900

    chore: user id 변경

--- src/main/java/com/shinhan/bullsandbears/domain/User.java
+++ src/main/java/com/shinhan/bullsandbears/domain/User.java
@@ -5,7 +5,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
@@ -26,8 +29,11 @@ import lombok.Setter;
 public class User {
 
 	@Id
-	private String email;
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_id")
+	private Long id;
 
+	private String email;
 	private String name;
 	private String provider;
 
