2d26c94b7cb0302cd942a74dd2685019be501c0d
gyo <dldlsry053@naver.com>
2023-10-19 23:57:32 +0900

    DSP-main-hotfix-외부-api-호출-주석처리

--- src/main/java/com/shinhan/bullsandbears/controller/ExternalController.java
+++ src/main/java/com/shinhan/bullsandbears/controller/ExternalController.java
@@ -1,21 +1,21 @@
-package com.shinhan.bullsandbears.controller;
-
-import com.shinhan.bullsandbears.externalApi.ExternalApiUseCase;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequiredArgsConstructor
-@RequestMapping("/api/v1/")
-public class ExternalController {
-
-    private final ExternalApiUseCase externalApiUseCase;
-
-    @GetMapping("/sinhan")
-    public String get() {
-        return externalApiUseCase.get();
-    }
-
-}
+//package com.shinhan.bullsandbears.controller;
+//
+//import com.shinhan.bullsandbears.externalApi.ExternalApiUseCase;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequiredArgsConstructor
+//@RequestMapping("/api/v1/")
+//public class ExternalController {
+//
+//    private final ExternalApiUseCase externalApiUseCase;
+//
+//    @GetMapping("/sinhan")
+//    public String get() {
+//        return externalApiUseCase.get();
+//    }
+//
+//}
--- src/main/java/com/shinhan/bullsandbears/externalApi/ExternalApiUseCase.java
+++ src/main/java/com/shinhan/bullsandbears/externalApi/ExternalApiUseCase.java
@@ -1,45 +1,45 @@
-package com.shinhan.bullsandbears.externalApi;
-
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-import org.springframework.stereotype.Service;
-
-@Service
-public class ExternalApiUseCase {
-
-    public String get() {
-        return callApi();
-    }
-
-    private String callApi() {
-        OkHttpClient client = new OkHttpClient().newBuilder()
-                .hostnameVerifier((hostname, session) -> {
-                    // if ("trustedhostname.com".equals(hostname)) {
-                    //     return true;
-                    // } else {
-                    //     return false;
-                    // }
-                    return true; // 모든 호스트를 허용하는 예제
-                })
-                .build();
-
-        Request request = new Request.Builder()
-                .url("https://gapi.shinhansec.com:8443/openapi/v1.0/strategy/market-issue")
-                .method("GET", null)
-                .addHeader("apiKey", "l7xxR7Fe0dP3i8KPZaPKpknI2vWrMeJfwDpk") // apiKey를 추가합니다.
-                .build();
-
-        try {
-            Response response = client.newCall(request).execute();
-            if (response.isSuccessful() && response.body() != null) {
-                return response.body().string();
-            } else {
-                System.err.println("Request failed: " + response.code());
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return "Request failed!";
-    }
-}
+//package com.shinhan.bullsandbears.externalApi;
+//
+//import okhttp3.OkHttpClient;
+//import okhttp3.Request;
+//import okhttp3.Response;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class ExternalApiUseCase {
+//
+//    public String get() {
+//        return callApi();
+//    }
+//
+//    private String callApi() {
+//        OkHttpClient client = new OkHttpClient().newBuilder()
+//                .hostnameVerifier((hostname, session) -> {
+//                    // if ("trustedhostname.com".equals(hostname)) {
+//                    //     return true;
+//                    // } else {
+//                    //     return false;
+//                    // }
+//                    return true; // 모든 호스트를 허용하는 예제
+//                })
+//                .build();
+//
+//        Request request = new Request.Builder()
+//                .url("https://gapi.shinhansec.com:8443/openapi/v1.0/strategy/market-issue")
+//                .method("GET", null)
+//                .addHeader("apiKey", "l7xxR7Fe0dP3i8KPZaPKpknI2vWrMeJfwDpk") // apiKey를 추가합니다.
+//                .build();
+//
+//        try {
+//            Response response = client.newCall(request).execute();
+//            if (response.isSuccessful() && response.body() != null) {
+//                return response.body().string();
+//            } else {
+//                System.err.println("Request failed: " + response.code());
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        return "Request failed!";
+//    }
+//}
